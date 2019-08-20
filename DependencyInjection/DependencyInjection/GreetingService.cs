2b8bc363159168dc0106d4c022d77ef6b4e8daa4
Andrea Marasovic <jupitelko@gmail.com>
2019-08-20 18:28:03 +0200

    Added changes.

---
+++ DependencyInjection/DependencyInjection/GreetingService.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Microsoft.Extensions.Options;
+
+namespace DependencyInjection
+{
+    public class GreetingServiceOptions
+    {
+        public string From { get; set; }
+        public string To { get; set; }
+    }
+
+    public class GreetingService : IGreetingService
+    {
+        public GreetingService(IOptions<GreetingServiceOptions> options)
+        {
+            this._from = options.Value.From;
+            this._to = options.Value.To;
+        }
+
+        private string _from;
+        private string _to;
+
+        public string Greeting(string name)
+        {
+            if (name == null) throw new ArgumentNullException(nameof(name));
+
+            return $"Hello, {name}, greetings from {_from} to {_to}";
+        }
+    }
+}
---
+++ DependencyInjection/DependencyInjection/HelloController.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DependencyInjection
+{
+    public class HelloController
+    {
+        private readonly IGreetingService _greetingService;
+        public HelloController(IGreetingService greetingService) =>
+            _greetingService = greetingService;
+
+        public string Action(string name) =>
+            _greetingService.Greeting(name);
+    }
+}
